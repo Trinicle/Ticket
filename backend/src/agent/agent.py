06683f13376c3fac4eb41b51debfe32736fc332e
Hunter <hunter.freeland2003@gmail.com>
2026-02-08 02:11:44 -0500

    setup tools that may be needed

---
+++ backend/src/agent/agent.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from typing import Callable
+from langchain.agents import create_agent
+from langchain.agents.middleware import ModelRequest, ModelResponse, wrap_model_call
+from langgraph.checkpoint.memory import InMemorySaver
+from backend.src.agent.tools.github import github_tools, GITHUB_SYSTEM_PROMPT, IssueState
+
+@dataclass
+class TaskContext:
+    platform: str
+    token: str
+    repository: str
+
+
+@wrap_model_call
+def change_available_tools(request: ModelRequest, handler: Callable[[ModelRequest], ModelResponse]) -> ModelResponse:
+    system_prompt = request.runtime.context.get("system_prompt")
+    platform = request.runtime.context.get("platform")
+    state = request.runtime.state
+    tools = request.tools
+    
+    if platform == "github":
+        system_prompt = GITHUB_SYSTEM_PROMPT
+        tools = github_tools
+        state = IssueState
+        
+    return handler(request.override(tools=tools, system_message=system_prompt, state=state))
+
+
+def create_rag_agent():
+    return create_agent(
+        model="gpt-4o-mini",
+        system_prompt=GITHUB_SYSTEM_PROMPT,
+        tools = github_tools, 
+        state_schema=IssueState,
+        context_schema=TaskContext,
+        middleware = [change_available_tools],
+        checkpointer=InMemorySaver()
+    )
---
+++ backend/src/agent/tools/github.py
@@ -0,0 +1,163 @@
+from datetime import date, timedelta
+from typing import List, Optional
+import httpx
+from langchain.agents import AgentState
+from langchain.tools import ToolRuntime, tool
+from langchain_core.messages import SystemMessage
+
+from backend.src.agent.agent import TaskContext
+
+
+GITHUB_SYSTEM_PROMPT = SystemMessage(
+    content="""
+You are a helpful assistant that can investigate, create, modify, and delete GitHub issues and Comments.
+"""
+)
+
+
+class IssueState(AgentState):
+    """Context about a GitHub issue for RAG"""
+
+    issue_number: int
+    title: str
+    body: str
+    state: str
+    created_at: str
+    updated_at: str
+    comments_count: int
+    labels: List[str]
+    assignees: List[str]
+    repository: str
+
+
+BASE_URL = "https://api.github.com"
+
+
+@tool
+async def search_issues(
+    runtime: ToolRuntime[TaskContext],
+    title: Optional[str] = None,
+    body: Optional[str] = None,
+    days_ago: Optional[int] = 30,
+):
+    """Searches for issues based on the title and body
+
+    Args:
+        title: Optional search terms to look for in the title
+        body: Optional search terms to look for in the body
+        days_ago: Optional number of days ago to search for issues
+    """
+    repository = runtime.context.repository
+
+    query = [
+        f"repo:{repository}",
+        f"created:>{date.today() - timedelta(days=days_ago)}",
+    ]
+
+    if title:
+        query.append(f"title:{title}")
+
+    if body:
+        query.append(f"body:{body}")
+
+    params = {
+        "q": " ".join(query),
+        "per_page": 15,
+        "page": 1,
+    }
+
+    async with httpx.AsyncClient(headers=get_default_headers()) as client:
+        response = await client.get(f"{BASE_URL}/search/issues", params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        simplified_issues = []
+
+        for issue in data.get("items", []):
+            simplified_issue = {
+                "number": issue.get("number"),
+                "title": issue.get("title"),
+                "body": (
+                    issue["body"][:500] + "..."
+                    if len(issue.get("body", "")) > 500
+                    else issue.get("body", "")
+                ),
+                "state": issue.get("state"),
+                "html_url": issue.get("html_url"),
+                "created_at": issue.get("created_at"),
+                "updated_at": issue.get("updated_at"),
+                "comments_count": issue.get("comments"),
+                "labels": [label.get("name") for label in issue.get("labels", [])],
+                "author": issue.get("user", {}).get("login"),
+                "assignee": (
+                    issue.get("assignee", {}).get("login")
+                    if issue.get("assignee")
+                    else None
+                ),
+            }
+            simplified_issues.append(simplified_issue)
+
+        return {
+            "total_count": data.get("total_count"),
+            "issues": simplified_issues,
+        }
+
+
+@tool
+def create_issue(runtime: ToolRuntime[TaskContext]):
+    pass
+
+
+@tool
+def update_issue(runtime: ToolRuntime[TaskContext]):
+    pass
+
+
+@tool(description="Gets the information about a specific issue")
+def get_issue(runtime: ToolRuntime[TaskContext]):
+    pass
+
+
+@tool
+def get_issue_comments(runtime: ToolRuntime[TaskContext]):
+    pass
+
+
+@tool
+def add_comment(runtime: ToolRuntime[TaskContext]):
+    pass
+
+
+@tool
+def update_comment(runtime: ToolRuntime[TaskContext]):
+    pass
+
+
+@tool
+def delete_comment(runtime: ToolRuntime[TaskContext]):
+    pass
+
+
+def get_state(runtime: ToolRuntime[TaskContext]) -> IssueState:
+    return runtime.state
+
+
+def get_token(runtime: ToolRuntime[TaskContext]):
+    return runtime.context.token
+
+
+def get_default_headers():
+    return {
+        "Accept": "application/vnd.github.v3+json",
+    }
+
+
+def get_auth_headers(runtime: ToolRuntime[TaskContext]):
+    return {
+        "Authorization": f"Bearer {get_token(runtime)}",
+        "Accept": "application/vnd.github.v3+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+
+
+github_tools = []
